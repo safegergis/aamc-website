0b5800cf618f4ab4cc52ab2d778031553a2477a8
Joe Ragheb <JoeRagheb@Joes-MacBook-Pro.local>
2018-03-31 11:26:42 -0700

    another commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
